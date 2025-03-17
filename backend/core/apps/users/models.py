4a6e11aa902d5deacb41b3b2293d1339e4134d69
OKnischay <nischaybasi015@gmail.com>
2025-03-17 11:37:36 +0545

    fix:changes in custom user models

--- backend/core/apps/users/models.py
+++ backend/core/apps/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from users.choices import GenderChoices,RoleChoices
+from core.base.choices import GenderChoices,RoleChoices
 from django.utils import timezone
-from users.managers import CustomUserManager
+from apps.users.managers import CustomUserManager
 # Create your models here.
 class CustomUser(AbstractBaseUser,PermissionsMixin):
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
+    # username = models.CharField(max_length=255, unique=True)
     phone = models.CharField(max_length=10,blank=True,null=True, unique=True)
     email = models.EmailField(unique=True)
     address = models.CharField(max_length=255)
@@ -21,12 +22,13 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
         max_length = 20
         )
 
-    date_of_birth = models.DateTimeField()
+    date_of_birth = models.DateField(null=True)
     
     date_joined = models.DateTimeField(auto_now_add=timezone.now())
     updated_at = models.DateTimeField(auto_now=timezone.now())
 
     is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     object = CustomUserManager()
