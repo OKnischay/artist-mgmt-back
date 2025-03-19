e6d097bb51fd2e2476f7f2579564d11c4746160f
OKnischay <nischaybasi015@gmail.com>
2025-03-19 09:34:55 +0545

    feat:implement crud in users

--- backend/core/apps/users/models.py
+++ backend/core/apps/users/models.py
@@ -1,40 +1,57 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from core.base.choices import GenderChoices,RoleChoices
-from django.utils import timezone
+# from django.utils import timezone
 from apps.users.managers import CustomUserManager
+from core.base.choices import GenderChoices, RoleChoices
+from core.base.models import BaseModel, Profile
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+
 # Create your models here.
-class CustomUser(AbstractBaseUser,PermissionsMixin):
+class CustomUser(AbstractBaseUser, PermissionsMixin, BaseModel):
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
     # username = models.CharField(max_length=255, unique=True)
-    phone = models.CharField(max_length=10,blank=True,null=True, unique=True)
+    phone = models.CharField(max_length=10, blank=True, null=True, unique=False)
     email = models.EmailField(unique=True)
     address = models.CharField(max_length=255)
 
     gender = models.CharField(
         choices=GenderChoices,
-        max_length=10
-        )
+        max_length=10)
+
     role = models.CharField(
-        choices=RoleChoices,
-        default=RoleChoices.artist,
-        max_length = 20
-        )
+        choices=RoleChoices, 
+        default=RoleChoices.artist, 
+        max_length=20
+    )
 
     date_of_birth = models.DateField(null=True)
-    
-    date_joined = models.DateTimeField(auto_now_add=timezone.now())
-    updated_at = models.DateTimeField(auto_now=timezone.now())
+
+    # date_joined= models.DateTimeField(auto_now_add=timezone.now())
+    # updated_at = models.DateTimeField(auto_now=timezone.now())
 
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    object = CustomUserManager()
+    objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        if self.first_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return f"{self.email}"
+
+class UserProfile(Profile):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "User Profile"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- backend/core/apps/users/views.py
+++ backend/core/apps/users/views.py
@@ -1,15 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 from .models import CustomUser
 from .serializers import UserSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-# Create your views here.
+from .selectors import UserSelectors
+from .services import UserServices
+
+user_services = UserServices()
+user_selectors = UserSelectors()
 
 class UserListAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
+    authentication_classes = []
+   
+    def get(self, request):
+        response = user_selectors.get_users()
+        return response
 
-    def get(self,request):
-        user = CustomUser.objects.all()
-        serializer = UserSerializer(many=True)
+    def post(self, request):
+        response = user_services.create_user(request.data)
+        return response
+
+
+class UserListDetailAPIView(APIView):
+    def get(self, request, pk):
+        response = user_selectors.get_user_detail(id=pk)
+        return response    
+
+    def put(self, request, pk):
+            response = user_services.update_user(request.data, id=pk)
+            return response
+
+    def delete(self, request, pk):
+        response = user_services.delete_user(id=pk)
+        return response
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exceptions=True)
+        serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+
+        user = CustomUser.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed("User not found")
+
+        if user.check_password(password):
+            raise AuthenticationFailed("Incorrect Password")
+
+        return Response({"message": "Login success"})
